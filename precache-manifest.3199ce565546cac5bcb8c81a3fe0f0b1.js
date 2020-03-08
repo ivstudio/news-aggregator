6130e29555c3eefb78ce1b232b1cc3bc54bb99a3
Iggy Villamar <iggy@ivstudio.com>
2020-03-08 17:51:10 -0400

    Updates

---
+++ precache-manifest.3199ce565546cac5bcb8c81a3fe0f0b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e392abc756cc72ab3a85b9a106d7120",
+    "url": "/news/index.html"
+  },
+  {
+    "revision": "0ed1273d1bcd14c18200",
+    "url": "/news/static/css/2.2623327d.chunk.css"
+  },
+  {
+    "revision": "c7f46b1bc6aba0d8ff4f",
+    "url": "/news/static/css/main.532dc474.chunk.css"
+  },
+  {
+    "revision": "0ed1273d1bcd14c18200",
+    "url": "/news/static/js/2.41364dbf.chunk.js"
+  },
+  {
+    "revision": "f14821cec6185528aee9ce0fd4ed107f",
+    "url": "/news/static/js/2.41364dbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7f46b1bc6aba0d8ff4f",
+    "url": "/news/static/js/main.6da5227a.chunk.js"
+  },
+  {
+    "revision": "4ffeff533dfb92d10efa",
+    "url": "/news/static/js/runtime-main.3800acec.js"
+  }
+]);
